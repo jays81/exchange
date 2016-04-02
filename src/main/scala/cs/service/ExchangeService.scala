a784e31088f7c31ffc35c750719263b5d56730f0
jay <jays81@yahoo.com>
2016-04-02 20:27:38 +0100

    match 2 orders

--- src/main/scala/cs/service/ExchangeService.scala
+++ src/main/scala/cs/service/ExchangeService.scala
@@ -1,6 +1,6 @@
 package cs.service
 
-import cs.Order
+import cs.{Direction, Order}
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -16,20 +16,41 @@ class ExchangeService {
     * against existing orders to check if it can be matched
     * If orders match they are said to be executed
     * returns true if an order has been executed
+    *
     * @param order
     * @return
     */
   def addOrder(order: Order): Boolean = {
-    false
+    openOrders += order
+    val ordersFound = openOrders.find(openOrder => matchOrder(order, openOrder))
+    println(ordersFound)
+
+    val executed = ordersFound match {
+      case Some(e) => true
+      case None => false
+    }
+
+    executed
   }
 
-  /**
+  private def checkPrice(orderToMatch: Order, openOrder: Order): Boolean = {
+    val checkPrice = orderToMatch.direction match {
+        case Direction.sell => orderToMatch.price <= openOrder.price
+        case _  => openOrder.price <= orderToMatch.price
+    }
+    checkPrice
+  }
+
+/**
     * Two orders match if they have opposing directions, matching RICs and quantities, and if the
     * sell price is less than or equal to the buy price
     */
   private def matchOrder(orderToMatch:Order, openOrder:Order):Boolean = {
-    //val doesOrderMatch = match
-    false
+    (orderToMatch.direction != openOrder.direction &&
+      orderToMatch.ric == openOrder.ric &&
+      orderToMatch.quantity == openOrder.quantity &&
+      checkPrice(orderToMatch, openOrder))
+
   }
 
 }
--- src/test/scala/cs/service/ExchangeServiceSpec.scala
+++ src/test/scala/cs/service/ExchangeServiceSpec.scala
@@ -9,12 +9,29 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class ExchangeServiceSpec extends FlatSpec with Matchers {
 
-  "ExchangeService" should "add an order" in {
+  "ExchangeService" should "add an order and should not have been executed" in {
     val exchangeService = new ExchangeService
 
 
     val order = Order(Direction.sell, "VOD.L", 1000, 100.2, "User1")
-    exchangeService.addOrder(order)
+    val executed = exchangeService.addOrder(order)
+
+    executed should be (false)
+  }
+
+  it should "add an order and should have been executed" in {
+    val exchangeService = new ExchangeService
+
+
+    val order1 = Order(Direction.sell, "VOD.L", 1000, 100.2, "User1")
+    val order2 = Order(Direction.buy, "VOD.L", 1000, 100.2, "User2")
+    val executed1 = exchangeService.addOrder(order1)
+    //first add should not have been executed
+    executed1 should be (false)
+
+    val executed2 = exchangeService.addOrder(order2)
+    //first add should not have been executed
+    executed2 should be (true)
   }
 
 }
